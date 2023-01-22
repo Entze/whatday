ec1e77c51e99b2e8f3955c34fc6fd5c93cdd110e
Lukas Grassauer <lukas@grassauer.eu>
2023-01-22 16:36:07 +0100

    test(app.py): add rudementary test to check if strftime has the same output as implementation

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,7 @@
+import datetime
 import unittest
 
-from whatday.app import get_century_drift
+from whatday.app import get_century_drift, calculate_weekday
 
 
 class TestGetCenturyDrift(unittest.TestCase):
@@ -45,3 +46,96 @@ class TestGetCenturyDrift(unittest.TestCase):
                 expected = 5
                 actual, _ = get_century_drift(year)
                 self.assertEqual(expected, actual)
+
+
+class TestCalculateWeekday(unittest.TestCase):
+    def test_same_as_strftime_seventeenth_century(self):
+        for year in range(1600, 1700):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_eighteenth_century(self):
+        for year in range(1700, 1800):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_nineteenth_century(self):
+        for year in range(1800, 1900):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_twentieth_century(self):
+        for year in range(1900, 2000):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_twenty_first_century(self):
+        for year in range(2000, 2100):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_twenty_second_century(self):
+        for year in range(2100, 2200):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
+
+    def test_same_as_strftime_twenty_third_century(self):
+        for year in range(2200, 2300):
+            for month in range(1, 13):
+                for day in range(1, 32):
+                    try:
+                        datetime.date(year, month, day)
+                    except ValueError:
+                        continue
+                    with self.subTest(year=year, month=month, day=day):
+                        expected = datetime.date(year, month, day).strftime("%A")
+                        actual, _ = calculate_weekday(year, month, day)
+                        self.assertEqual(expected, actual)
